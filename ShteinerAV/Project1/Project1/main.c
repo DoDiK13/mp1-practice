857f5df9861a7880e91dd8076566b011507643bc
DoDiK13 <aleksandrstejner905@gmail.com>
2025-10-27 19:34:35 +0300

    Algoritms

--- ShteinerAV/Project1/Project1/main.c
+++ ShteinerAV/Project1/Project1/main.c
@@ -1,114 +1,196 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <locale.h>
-#include <time.h>
+#define n 9
+//#define FIND_BAR
+//#define REVERSE
+//#define KOL_DIFFERENT
+//#define SAM_WORK
+//#define BIN_FIND
+//#define KOL_SIMILAR
+//#define ARRAY_DIFFERENT
 
-#define DEBUG
-
-int* user_input() {
-	int static arr[5];
-
-	for (int i = 0; i < 5; i++) {
-		printf("Число %d: ", i + 1);
-		scanf_s("%d", &arr[i]);
-		getchar();
-
-		while (arr[i] < 0 || arr[i] > 9) {
-			printf("\nТы ввёл число неверно! Попробуй ещё раз.\n");
-			printf("Число %d: ", i + 1);
-			scanf_s("%d", &arr[i]);
-			getchar();
-		}
-	}
-
-	return arr;
-}
-
-int find(int arr[], int size, int target) {
-	int i;
-	for (i = 0; i < size; i++) {
-		if (arr[i] == target) {
-			return 1;
-		}
-	}
-
-	return 0;
-}
 
 int main() {
 	setlocale(LC_ALL, "rus");
-	srand((unsigned int)time(0));
+#ifdef FIND_BAR
+	// ПОИСК ЭЛЕМЕНТОВ С БАРЬЕРОМ
+	int arr[n + 1], i, target, orig_elem;
 
-	int digget[10] = { 0,0,0,0,0,0,0,0,0,0 };
-	int generate_arr[5];
-	int is_play = 1, cnt_try = 0, len;
-
-	len = 1 + rand() % 4;
-
-	//Генерация чисел с рандомными неповторяющимися элементами
-	for (int i = 0; i < len; i++) {
-		int number = rand() % 10;
-		if (i == 0 && number == 0) {
-			number = 1 + rand() % 9;
-		}
-		while (digget[number] != 0) {
-			number = rand() % 10;
-		}
-		digget[number] = 1;
-		generate_arr[i] = number;
+	printf("Введи %d символов в список:\n", n);
+	for (i = 0; i < n; i++) {
+		scanf_s("%d", &arr[i]);
 	}
 
-	int user_num;
-	int user_arr[len];
+	printf("Введите число, которое будем искать: ");
+	scanf_s("%d", &target);
 
-	scanf_s("%d", &user_num);
+	arr[n] = target;
 
-	int i = len - 1;
-	while (user_num != 0) {
-		user_arr[i--] = user_num % 10;
-		user_num /= 10;
+	i = 0;
+	while (arr[i] != target) {
+		i++;
 	}
 
-
-
-#ifdef DEBUG
-	printf("***Загаданная комбинация - ");
-	for (int i = 0; i < 5; i++) {
-		printf("%d", generate_arr[i]);
+	if (i < n - 1) {
+		printf("%d", i);
 	}
-	printf("***\n--------------------------------------\n");
-#endif 
+	else {
+		printf("Нет элемента");
+	}
+#endif
 
-	do {
-		int cnt_bulls = 0, cnt_cows = 0;
-		cnt_try += 1;
+#ifdef REVERSE
+	int arr[n];
 
-		for (int i = 0; i < 5; i++) {
+	printf("Введите %d элементов\n", n);
+	for (int i = 0; i < n; i++) {
+		scanf_s("%d", &arr[i]);
+	}
 
-			if (generate_arr[i] == user_nums[i]) {
-				cnt_bulls += 1;
+	for (int i = 0; i < n / 2; i++) {
+		int buf = arr[i];
+		arr[i] = arr[n - 1 - i];
+		arr[n - 1 - i] = buf;
+	}
+
+	for (int i = 0; i < n; i++) {
+		printf("%d", arr[i]);
+	}
+
+#endif
+
+#ifdef KOL_DIFFERENT
+	int arr[n], cnt = 1;
+
+	printf("Введите %d элементов\n", n);
+	for (int i = 0; i < n; i++) {
+		scanf_s("%d", &arr[i]);
+	}
+
+	for (int i = 1; i < n; i++) {
+		int flag = 1;
+		for (int j = 0; j < i; j++) {
+			if (arr[j] == arr[i]) {
+				flag = 0;
+				break;
 			}
-			else if (find(generate_arr, 5, user_nums[i])) {
-				cnt_cows += 1;
+		}
+		cnt += flag;
+	}
+
+	printf("Кол-во различных чисел: %d", cnt);
+#endif
+
+#ifdef SAM_WORK
+	int num1, num2 = 0;
+	int arr[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+	printf("Введи число: ");
+	scanf_s("%d", &num1);
+
+	while (num1 > 0) {
+		arr[(num1 % 10) - 1]++;
+		num1 /= 10;
+	}
+
+	for (int i = 8; i >= 0; i--) {
+		num2 += arr[i];
+		num2 *= 10;
+	}
+
+	num2 /= 10;
+	printf("%d", num2);
+#endif
+
+#ifdef BIN_FIND
+	int arr[n], i, right = n - 1, left = 0, mid, target, id = -1;
+
+	printf("Введи %d символов в список:\n", n);
+	for (i = 0; i < n; i++) {
+		scanf_s("%d", &arr[i]);
+	}
+
+	printf("Введи искомое число: ");
+	scanf_s("%d", &target);
+
+	while (left <= right) {
+		mid = left + (right - left) / 2;
+
+		if (arr[mid] == target) {
+			id = mid;
+			break;
+		}
+		else if (arr[mid] < target) {
+			left = mid + 1;
+		}
+		else {
+			right = mid - 1;
+		}
+	}
+
+	if (id >= 0) {
+		printf("Число %d находится на индексе %d", arr[id], id);
+	}
+	else {
+		printf("Такого числа нету!");
+	}
+
+#endif
+
+#ifdef KOL_SIMILAR
+	int arr[n];
+
+	printf("Введите %d элементов\n", n);
+	for (int i = 0; i < n; i++) {
+		scanf_s("%d", &arr[i]);
+	}
+
+	for (int i = 0; i < n; i++) {
+		int cnt = 1, flag = 1;
+		for (int j = 0; j < i; j++) {
+			if (arr[i] == arr[j]) {
+				flag = 0;
+				break;
 			}
-
 		}
 
-		if (cnt_bulls == 5) {
-			is_play = 0;
+		if (flag) {
+			for (int j = i + 1; j < n; j++) {
+				if (arr[i] == arr[j]) {
+					cnt++;
+				}
+			}
+			printf("%d - %d ", arr[i], cnt);
+		}
+	}
+#endif
+
+#ifdef ARRAY_DIFFERENT
+	int arr[n], new_arr[n], id_n = 0;
+
+	printf("Введите %d элементов\n", n);
+	for (int i = 0; i < n; i++) {
+		scanf_s("%d", &arr[i]);
+	}
+
+	for (int i = 0; i < n; i++) {
+		int flag = 1;
+		for (int j = 0; j < i; j++) {
+			if (arr[i] == arr[j]) {
+				flag = 0;
+				break;
+			}
 		}
 
-		printf("\n");
-		for (int i = 0; i < 5; i++) {
-			printf("%d", user_nums[i]);
+		if (flag) {
+			new_arr[id_n++] = arr[i];
 		}
+	}
 
-		printf("\nКол-во быков: %d\n", cnt_bulls);
-		printf("Кол-во коров: %d\n--------------------------------------\n", cnt_cows);
+	for (int i = 0; i < id_n; i++) {
+		printf("%d", new_arr[i]);
+	}
+#endif
 
-	} while (is_play);
-
-	printf("Ты выйграл за %d попыток!", cnt_try);
-
-	return 777;
+	return 0;
 }
