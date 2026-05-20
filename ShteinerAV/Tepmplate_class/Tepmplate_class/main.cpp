222e227caecd82c437dd4981a4ebc0324f635680
DoDiK13 <aleksandrstejner905@gmail.com>
2026-05-20 09:23:20 +0300

    mur

--- ShteinerAV/Tepmplate_class/Tepmplate_class/main.cpp
+++ ShteinerAV/Tepmplate_class/Tepmplate_class/main.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
+#include "template.h"
+#include "vector.h"
 
 int main() {
 
+	try {
+	Container<int> int_c(5, 1);
+	Container <double> double_c(2, 2);
+
+	// заполнение
+	for (int i = 0; i < int_c.get_size(); i++) {
+		int_c.InsertElement(i + 3);
+	}
+	for (int i = 0; i < int_c.get_size() + 2; i++) {
+		double_c.InsertElement(i + 0.25);
+	}
 
 
+	// вывод
+	std::cout << int_c << std::endl;
+	std::cout << double_c << std :: endl;
+	
+	std::cout << "-----------------------------------------------------------\n";
+	// поиск 
+	std::cout << int_c.SearchElement(5) << std::endl;
+	std::cout << double_c.SearchElement(2.03) << std::endl;
+
+	std::cout << "-----------------------------------------------------------\n";
+	// удаление 
+	int_c.DeleteElement(5);
+	double_c.DeleteElement(3.25);
+
+	std::cout << int_c << std::endl;
+	std::cout << double_c << std::endl;
+	
+
+	}
+	catch (const std::exception& ex) {
+		std::cout << ex.what();
+	}
+	
 
 	return 0;
 }
--- ShteinerAV/Tepmplate_class/Tepmplate_class/template.h
+++ ShteinerAV/Tepmplate_class/Tepmplate_class/template.h
@@ -1,10 +1,12 @@
+
+
 template <class T>
 class Container {
 private:
     T* elems;
     int size;
     int count = 0;
-    int step = 1; 
+    unsigned int step; 
 public:
     Container(int, int);
     Container(const Container <T>&);
@@ -14,12 +16,25 @@ public:
     void DeleteElement(const T&);
     T operator [] (int) const;
     const Container<T>& operator = (const Container<T>&);
+    friend std::ostream& operator << (std::ostream& os, const Container<T>& c) {
+        os << "[";
+        for (int i = 0; i < c.count; i++) {
+            os << c[i] << " ";
+        }
+        os << "]";
+        return os;
+    }
+
+
+    int get_size() {
+        return this->size;
+    }
 };
 
 template <class T>
 Container<T>::Container(int size, int step) {
     if (step <= 0) {
-        throw std::exceprion("no valid step")
+        throw std::exception("no valid step");
     }
     elems = new T[size];
     this->size = size;
@@ -99,6 +114,124 @@ const Container<T>& Container<T>::operator = (const Container<T>& temp) {
     this->count = temp.count;
     for (int i = 0; i < count; i++) {
         this->elems[i] = temp.elems[i]; 
+    }
+
+    return *this;
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+template <class T>
+class Container<T*> {
+private:
+    T** elems;
+    int size;
+    int count = 0;
+    int step;
+public:
+    Container(int, int);
+    Container(const Container<T*>&);
+    ~Container();
+    void InsertElement(T* const&);
+    int SearchElement(T* const&) const;
+    void DeleteElement(T* const&);
+    T* operator [] (int) const;
+    const Container<T*>& operator = (const Container<T*>&);
+
+    int get_size() {
+        return this->size;
+    }
+
+};
+
+template <class T>
+Container<T*>::Container(int size, int step) {
+    if (step <= 0) {
+        throw std::exception("no valid step");
+    }
+    elems = new T * [size];
+    this->size = size;
+    this->step = step;
+}
+
+template <class T>
+Container<T*>::Container(const Container<T*>& temp) {
+    this->size = temp.size;
+    this->step = temp.step;
+    this->elems = new T * [this->size];
+    this->count = temp.count;
+    for (int i = 0; i < this->count; i++) {
+        this->elems[i] = temp.elems[i];  
+    }
+}
+
+template <class T>
+Container<T*>::~Container() {
+    for (int i = 0; i < count; i++) {
+        delete[] elems[i];
+    }
+    delete[] elems;
+}
+
+template <class T>
+void Container<T*>::InsertElement(T* const& elem) {
+    if (count == size) {
+        Container<T*> temp(size + step, step);
+        temp.count = this->count;
+
+        for (int i = 0; i < temp.count; i++) {
+            temp.elems[i] = this->elems[i];
+        }
+
+        (*this) = temp;
+    }
+
+    this->elems[count++] = elem;
+}
+
+template <class T>
+int Container<T*>::SearchElement(T* const& elem) const {
+    for (int i = 0; i < count; i++) {
+        if (elems[i] == elem) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+template <class T>
+void Container<T*>::DeleteElement(T* const& elem) {
+    int index = SearchElement(elem);
+    if (index == -1) {
+        throw std::exception("no find");
+    }
+    elems[index] = elems[--count];
+}
+
+template <class T>
+T* Container<T*>::operator [] (int index) const {
+    if (index < 0 || index > count - 1) {
+        throw std::exception("no index");
+    }
+    return elems[index];
+}
+
+template <class T>
+const Container<T*>& Container<T*>::operator = (const Container<T*>& temp) {
+    if (this == &temp) {
+        return *this;
+    }
+
+    if (this->size != temp.size) {
+        this->size = temp.size;
+        this->step = temp.step;
+        delete[] this->elems;
+        this->elems = new T * [size];
+    }
+
+    this->count = temp.count;
+    for (int i = 0; i < count; i++) {
+        this->elems[i] = temp.elems[i];
     }
 
     return *this;
